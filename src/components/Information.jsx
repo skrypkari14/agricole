51a3b3802461b0079c570ad6636c62107e827ce3
c0der <tgpop2000@gmail.com>
2024-07-01 12:44:29 +0300

    fix v7

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -17,66 +17,66 @@ const Information = () => {
 
     return (
         <div className='containerBank'>
-            <div className='bg-white rounded-t-xl px-7 py-2 flex flex-col items-center justify-between gap-4'>
-                <div className='gridContainer overflow-hidden'>
-                    <span className='h-6 min-w-6 bg-[#C1221F] text-white text-center rounded-full text-sm'>
-                        !
-                    </span>
-                    <p className='text-[#C1221F] font-semibold text-sm min-h-9 text-left md:text-center flex items-center w-full'>
-                        Uważaj: oszuści podają się za pracowników banku
-                    </p>
-                    <div></div>
-                    <div className={
-                        `collaps text-sm ${
-                            isOpenWhite ? 'open' : ''
-                        }`
-                    }>
-                        <p className='font-semibold mb-4'>Uważaj na oszustów, którzy przez telefon:</p>
-                        <ul className='pt-[40px]'>
-                            <li>
-                                <strong>podają się za pracowników</strong>
-                                banków lub instytucji finansowych
-                                <br/>
-                                (na telefonie может wyświetлиć się номер, который выглядит как один из номеров bankowych)
-                            </li>
-                            <li>chcą Cię przestraszyć
-                                <strong>zmyśloną historią o podejrzanym przelewie lub kredycie</strong>
-                            </li>
-                            <li>mogą zapewniać, że współpracują z policją i zależy im, żeby
-                                <strong>działać szybko</strong>
-                            </li>
-                            <li>namawiają do
-                                <strong>zainstalowania programu</strong>
-                                na smartfonie lub komputerze</li>
-                            <li>chcą od Ciebie poufnych informacji</li>
-                            <li>nakłaniają do wykonania operacji bankowych, также przekazania pieniędzy na tzw. „bezpieczne konto”, wypłaty gotówki i jej ponownej wpłaty we wpłatomacie</li>
-                        </ul>
-                        <p className='pt-[30px]'>
-                            <strong>Nie daj się złowić. To próba kradzieży Twoich pieniędzy.</strong>
-                        </p>
-                        <p className='pt-[30px]'>Przestępca zwykle wie, jak się nazywasz. Czasami, żeby się uwiarygodnić, może w trakcie rozmowy lub chwilę przed nią wysłać SMS-a, który wygląda jak wiadomość z banku, albo podać jakieś dodatkowe informacje np. kilka ostatnich cyfr numeru Twojej karty płatniczej.</p>
-                        <p className='pt-[30px]'>Oszuści często namawiają też do zainstalowania oprogramowania, które da им możliwość samodzielnego wykonywania operacji на Twoim koncie.</p>
-                        <p className='pt-[30px]'>
-                            <strong>Niczego nie instaluj, не podawaj poufnych danych, nie wykonuj żadnych operacji.</strong>
-                            Najlepiej jak najszybciej się rozłącz. Jeśli chcesz się upewnić, że z Twoim rachunkiem wszystko w porządku, skontaktuj się с nami.
-                        </p>
-                        <p className='py-[30px]'>Przeczytaj też artykuł на naszym blogu CAsfera i bankuj bezpiecznie
-                            <a className='text-[#007e90] hover:underline' href="https://www.casfera.pl/oszustwa-na-pracownika-banku/">https://www.casfera.pl/oszustwa-na-pracownika-banku/</a>
-                        </p>
+            <div>
+                <div className='bg-white rounded-t-xl px-7 py-2 flex flex-col md:flex-row items-center justify-between gap-4'>
+                    <div className='gridContainer overflow-hidden'>
+                        <span className='h-6 min-w-6 bg-[#C1221F] text-white text-center rounded-full text-sm'>
+                            !
+                        </span>
+                        <p className='text-[#C1221F] font-semibold text-sm min-h-9 text-left md:text-center flex items-center'>Uważaj: oszuści podają się za pracowników banku</p>
+                        <div></div>
+                        <div className={
+                            `collaps text-sm ${
+                                isOpenWhite ? 'open' : ''
+                            }`
+                        }>
+                            <p className='font-semibold mb-4'>Uważaj na oszustów, którzy przez telefon:</p>
+                            <ul className='pt-[40px]'>
+                                <li>
+                                    <strong>podają się za pracowników</strong>
+                                    banków lub instytucji finansowych
+                                    <br/>
+                                    (na telefonie может wyświetлиć się номер, который выглядит как один из номеров bankowych)
+                                </li>
+                                <li>chcą Cię przestraszyć
+                                    <strong>zmyśloną historią o podejrzanym przelewie lub kredycie</strong>
+                                </li>
+                                <li>mogą zapewniać, że współpracują z policją i zależy im, żeby
+                                    <strong>działać szybko</strong>
+                                </li>
+                                <li>namawiają do
+                                    <strong>zainstalowania programu</strong>
+                                    na smartfonie lub komputerze</li>
+                                <li>chcą od Ciebie poufnych informacji</li>
+                                <li>nakłaniają do wykonania operacji bankowych, также przekazania pieniędzy na tzw. „bezpieczne konto”, wypłaty gotówki i jej ponownej wpłaty we wpłatomacie</li>
+                            </ul>
+                            <p className='pt-[30px]'>
+                                <strong>Nie daj się złowić. To próba kradzieży Twoich pieniędzy.</strong>
+                            </p>
+                            <p className='pt-[30px]'>Przestępca zwykle wie, jak się nazywasz. Czasami, żeby się uwiarygodnić, może w trakcie rozmowy lub chwilę przed nią wysłać SMS-a, który wygląda jak wiadomość z banku, albo podać jakieś dodatkowe informacje np. kilka ostatnich cyfr numeru Twojej karty płatniczej.</p>
+                            <p className='pt-[30px]'>Oszuści często namawiają też do zainstalowania oprogramowania, które da им możliwość samodzielnego wykonywania operacji на Twoim koncie.</p>
+                            <p className='pt-[30px]'>
+                                <strong>Niczego nie instaluj, не podawaj poufnych danych, nie wykonuj żadnych operacji.</strong>
+                                Najlepiej jak najszybciej się rozłącz. Jeśli chcesz się upewnić, że z Twoim rachunkiem wszystko w porządku, skontaktuj się с nami.
+                            </p>
+                            <p className='py-[30px]'>Przeczytaj też artykuł на naszym blogu CAsfera i bankuj bezpiecznie
+                                <a className='text-[#007e90] hover:underline' href="https://www.casfera.pl/oszustwa-na-pracownika-banku/">https://www.casfera.pl/oszustwa-na-pracownika-banku/</a>
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className='flex cursor-pointer min-w-[80px] ml-10 md:ml-4 self-start md:translate-y-[10px]'
-                    onClick={toggleCollapsible}>
-                    <p className='text-[#C1221F] font-semibold text-sm hover:underline'>
-                        {
-                        isOpenWhite ? 'Zwiń' : 'Rozwiń'
-                    } </p>
-                    <div className={
-                        `${
-                            isOpenWhite ? 'rotate-180' : 'rotate-0'
-                        }`
-                    }>
-                        <RedArrowDown/>
+                    <div className='flex cursor-pointer min-w-[80px] ml-10 md:ml-4 self-start md:translate-y-[10px]'
+                        onClick={toggleCollapsible}>
+                        <p className='text-[#C1221F] font-semibold text-sm hover:underline'>
+                            {
+                            isOpenWhite ? 'Zwiń' : 'Rozwiń'
+                        } </p>
+                        <div className={
+                            `${
+                                isOpenWhite ? 'rotate-180' : 'rotate-0'
+                            }`
+                        }>
+                            <RedArrowDown/>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -94,7 +94,8 @@ const Information = () => {
                             }`
                         }>
                             <p className='pb-[40px] text-white'>Chcesz zlecić przelew, zmienić limity transakcji, metodę autoryzacji, a może odblokować dostęp do aplikacji? Zobacz, jak łatwo możesz to zrobić w CA24 eBank. Przygotowaliśmy dla Ciebie podpowiedzi w sekcji
-                                <a href='#' className='text-[#007e90]'> Pytania i odpowiedzi</a>.</p>
+                                <a href='#' className='text-[#007e90]'>
+                                    Pytania i odpowiedzi</a>.</p>
                         </div>
                     </div>
                     <div className='flex cursor-pointer min-w-[80px] ml-10 md:ml-4 self-start md:translate-y-[10px]'
